35d3cc4643a87f81e7b9c3bf9da3ec22c83ec591
Deng Changdong <dengchangdong@gmail.com>
2023-09-11 21:03:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 	"strconv"
-	"strings" 
+	"strings"
 
 	"github.com/dengchangdong/DuckDuckGo-API/duckduckgo"
 	"github.com/dengchangdong/DuckDuckGo-API/typings"
@@ -15,10 +15,48 @@ func main() {
 	HOST := os.Getenv("HOST")
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
-	PORT = "8080"
+		PORT = "8080"
 	}
 	handler := gin.Default()
+	handler.GET("/search?ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+	handler.POST("/search", func(ctx *gin.Context) {
+		// Map request to Search struct
+		var search typings.Search
+		if err := ctx.ShouldBindJSON(&search); err != nil {
+			ctx.JSON(400, gin.H{"error": err.Error(), "details": "Could not bind JSON"})
+			return
+		}
+		// Ensure query is set
+		if search.Query == "" {
+			ctx.JSON(400, gin.H{"error": "Query is required"})
+			return
+		}
+		// Get results
+		results, err := duckduckgo.Get_results(search)
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		// Limit
+		if search.Limit > 0 && search.Limit < len(results) {
+			results = results[:search.Limit]
+		}
 
+		// Check if 'text' parameter is set to 'true'
+		if ctx.Query("text") == "true" {
+			// Merge snippet fields into a single text
+			var mergedSnippet string
+			for _, result := range results {
+				mergedSnippet += result.Snippet + " "
+			}
+			ctx.String(200, mergedSnippet)
+		} else {
+			// Return results as JSON
+			ctx.JSON(200, results)
+		}
+	})
 	handler.GET("/search", func(ctx *gin.Context) {
 		// Map request to Search struct
 		var search typings.Search
@@ -41,9 +79,6 @@ func main() {
 			}
 			search.Limit, _ = strconv.Atoi(limit)
 		}
-		// Get text parameter
-		search.Text = ctx.DefaultQuery("text", "false")
-
 		// Get results
 		results, err := duckduckgo.Get_results(search)
 		if err != nil {
@@ -54,15 +89,17 @@ func main() {
 		if search.Limit > 0 && search.Limit < len(results) {
 			results = results[:search.Limit]
 		}
-		// Check if we need to combine snippet fields
-		if search.Text == "true" {
-			var combinedSnippets []string
+
+		// Check if 'text' parameter is set to 'true'
+		if ctx.Query("text") == "true" {
+			// Merge snippet fields into a single text
+			var mergedSnippet string
 			for _, result := range results {
-				combinedSnippets = append(combinedSnippets, result.Snippet)
+				mergedSnippet += result.Snippet + " "
 			}
-			combinedText := strings.Join(combinedSnippets, " ")
-			ctx.String(200, combinedText)
+			ctx.String(200, mergedSnippet)
 		} else {
+			// Return results as JSON
 			ctx.JSON(200, results)
 		}
 	})
