3f4e896e637768a36a79f6e9c1a52420e4bfb3db
Deng Changdong <dengchangdong@gmail.com>
2023-09-11 20:29:21 +0800

    Update types.go

--- typings/types.go
+++ typings/types.go
@@ -5,7 +5,7 @@ type Search struct {
 	Region    string `json:"region"`
 	TimeRange string `json:"time_range"`
 	Limit     int    `json:"limit"`
-	Text 	  string `json:"text"`
+	Text      bool   `json:"text"`
 }
 
 type Result struct {
