2de0fbfc445b026f80b4f38cef452f162d7da39b
Deng Changdong <dengchangdong@gmail.com>
2023-09-11 20:36:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,100 +1,72 @@
 package main
 
 import (
-	"os"
-	"strconv"
-	"strings"
+  "os"
+  "strconv"
+  "strings" 
 
-	"github.com/dengchangdong/DuckDuckGo-API/duckduckgo"
-	"github.com/dengchangdong/DuckDuckGo-API/typings"
-	"github.com/acheong08/endless"
-	"github.com/gin-gonic/gin"
+  "github.com/dengchangdong/DuckDuckGo-API/duckduckgo"
+  "github.com/dengchangdong/DuckDuckGo-API/typings"
+  "github.com/acheong08/endless"
+  "github.com/gin-gonic/gin"
 )
 
 func main() {
-	HOST := os.Getenv("HOST")
-	PORT := os.Getenv("PORT")
-	if PORT == "" {
-		PORT = "8080"
-	}
-	handler := gin.Default()
-	handler.GET("/search?ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-	handler.POST("/search", func(ctx *gin.Context) {
-		// Map request to Search struct
-		var search typings.Search
-		if err := ctx.ShouldBindJSON(&search); err != nil {
-			ctx.JSON(400, gin.H{"error": err.Error(), "details": "Could not bind JSON"})
-			return
-		}
-		// Ensure query is set
-		if search.Query == "" {
-			ctx.JSON(400, gin.H{"error": "Query is required"})
-			return
-		}
-		// Get results
-		results, err := duckduckgo.Get_results(search)
-		if err != nil {
-			ctx.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-		// Limit
-		if search.Limit > 0 && search.Limit < len(results) {
-			results = results[:search.Limit]
-		}
+  HOST := os.Getenv("HOST")
+  PORT := os.Getenv("PORT")
+  if PORT == "" {
+    PORT = "8080"
+  }
+  handler := gin.Default()
 
-		// Return results
-		ctx.JSON(200, results)
-	})
-	handler.GET("/search", func(ctx *gin.Context) {
-		// Map request to Search struct
-		var search typings.Search
-		// Get query
-		search.Query = ctx.Query("query")
-		// Get region
-		search.Region = ctx.Query("region")
-		// Get time range
-		search.TimeRange = ctx.Query("time_range")
-		if search.Query == "" {
-			ctx.JSON(400, gin.H{"error": "Query is required"})
-			return
-		}
-		// Get limit and check if it's a number
-		limit := ctx.Query("limit")
-		if limit != "" {
-			if _, err := strconv.Atoi(limit); err != nil {
-				ctx.JSON(400, gin.H{"error": "Limit must be a number"})
-				return
-			}
-			search.Limit, _ = strconv.Atoi(limit)
-		}
-		// Get results
-		results, err := duckduckgo.Get_results(search)
-		if err != nil {
-			ctx.JSON(500, gin.H{"error": err.Error()})
-			return
-		}
-		// Shorten results to limit if limit is set
-		if search.Limit > 0 && search.Limit < len(results) {
-			results = results[:search.Limit]
-		}
+  handler.GET("/search", func(ctx *gin.Context) {
+    // Map request to Search struct
+    var search typings.Search
+    // Get query
+    search.Query = ctx.Query("query")
+    // Get region
+    search.Region = ctx.Query("region")
+    // Get time range
+    search.TimeRange = ctx.Query("time_range")
+    if search.Query == "" {
+      ctx.JSON(400, gin.H{"error": "Query is required"})
+      return
+    }
+    // Get limit and check if it's a number
+    limit := ctx.Query("limit")
+    if limit != "" {
+      if _, err := strconv.Atoi(limit); err != nil {
+        ctx.JSON(400, gin.H{"error": "Limit must be a number"})
+        return
+      }
+      search.Limit, _ = strconv.Atoi(limit)
+    }
+    // Get text parameter
+    text := ctx.Query("text", "false")
+    isText := strings.ToLower(text) == "true"
 
-		// Check if 'text' parameter is set to 'true'
-		search.Text := ctx.DefaultQuery("text", "false")
-		if search.Text == "true" {
-			// Combine snippet fields into a single text
-			var combinedText []string
-			for _, result := range results {
-				combinedText = append(combinedText, result.Snippet)
-			}
-			mergedSnippet := strings.Join(combinedText, " ")
-			ctx.String(200, mergedSnippet)
-		} else {
-			// Return results as JSON
-			ctx.JSON(200, results)
-		}
-	})
+    // Get results
+    results, err := duckduckgo.Get_results(search)
+    if err != nil {
+      ctx.JSON(500, gin.H{"error": err.Error()})
+      return
+    }
+    // Shorten results to limit if limit is set
+    if search.Limit > 0 && search.Limit < len(results) {
+      results = results[:search.Limit]
+    }
+    // Check if we need to combine snippet fields
+    if isText {
+      var combinedSnippets []string
+      for _, result := range results {
+        combinedSnippets = append(combinedSnippets, result.Snippet)
+      }
+      combinedText := strings.Join(combinedSnippets, " ")
+      ctx.String(200, combinedText)
+    } else {
+      ctx.JSON(200, results)
+    }
+  })
 
-	endless.ListenAndServe(HOST+":"+PORT, handler)
+  endless.ListenAndServe(HOST+":"+PORT, handler)
 }
