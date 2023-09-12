317c8a9a90af65a07c81e60d7cc280f0a5ce5151
Deng Changdong <dengchangdong@gmail.com>
2023-09-12 23:32:12 +0800

    Update main.go

--- main.go
+++ main.go
@@ -16,48 +16,64 @@ func main() {
 	if PORT == "" {
 		PORT = "8080"
 	}
+
 	handler := gin.Default()
-	handler.GET("/search/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
+
+	//handler.GET("/search/ping", func(c *gin.Context) {
+	//	c.String(200, "pong")
+	//})
+
 	handler.POST("/search", func(ctx *gin.Context) {
+
 		// Map request to Search struct
 		var search typings.Search
 		if err := ctx.ShouldBindJSON(&search); err != nil {
 			ctx.JSON(400, gin.H{"error": err.Error(), "details": "Could not bind JSON"})
 			return
 		}
+
 		// Ensure query is set
 		if search.Query == "" {
 			ctx.JSON(400, gin.H{"error": "Query is required"})
 			return
 		}
+
 		// Get results
 		results, err := duckduckgo.Get_results(search)
 		if err != nil {
 			ctx.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
+
 		// Limit
 		if search.Limit > 0 && search.Limit < len(results) {
 			results = results[:search.Limit]
 		}
-		// Return results
-		ctx.JSON(200, results)
+
+		// Create a JSON object with a "result" field containing the results
+		response := gin.H{"result": results}
+
+		// Return results as JSON
+		ctx.JSON(200, response)
 	})
 	handler.GET("/search", func(ctx *gin.Context) {
+
 		// Map request to Search struct
 		var search typings.Search
+
 		// Get query
 		search.Query = ctx.Query("query")
+
 		// Get region
 		search.Region = ctx.Query("region")
+
 		// Get time range
 		search.TimeRange = ctx.Query("time_range")
 		if search.Query == "" {
 			ctx.JSON(400, gin.H{"error": "Query is required"})
 			return
 		}
+
 		// Get limit and check if it's a number
 		limit := ctx.Query("limit")
 		if limit != "" {
@@ -67,18 +83,24 @@ func main() {
 			}
 			search.Limit, _ = strconv.Atoi(limit)
 		}
+
 		// Get results
 		results, err := duckduckgo.Get_results(search)
 		if err != nil {
 			ctx.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
+
 		// Shorten results to limit if limit is set
 		if search.Limit > 0 && search.Limit < len(results) {
 			results = results[:search.Limit]
 		}
-		// Return results
-		ctx.JSON(200, results)
+
+		// Create a JSON object with a "result" field containing the results
+		response := gin.H{"result": results}
+		
+		// Return results as JSON
+		ctx.JSON(200, response)
 	})
 
 	endless.ListenAndServe(HOST+":"+PORT, handler)
