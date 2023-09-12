37e3db3bb50e035be7be24b99203c150aa961401
Deng Changdong <dengchangdong@gmail.com>
2023-09-12 20:34:32 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,56 +17,43 @@ func main() {
 		PORT = "8080"
 	}
 	handler := gin.Default()
-
 	handler.GET("/search/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
+	handler.POST("/search", func(ctx *gin.Context) {
+		// Check if chatmod parameter is set to true
+		chatmod := ctx.DefaultQuery("chatmod", "no")
+
+		if chatmod == "true" {
+			// Handle chatmod true case
+			handleChatmodTrue(ctx)
+			return
+		}
+
+		// Handle chatmod false or not provided case (original format)
+		handleOriginalFormat(ctx)
+	})
 
 	handler.GET("/search", func(ctx *gin.Context) {
-		chatmodParam := ctx.Query("chatmod")
+		// Check if chatmod parameter is set to true
+		chatmod := ctx.DefaultQuery("chatmod", "no")
 
-		// Check the chatmod parameter
-		if chatmodParam == "true" {
+		if chatmod == "true" {
+			// Handle chatmod true case
 			handleChatmodTrue(ctx)
-		} else {
-			handleChatmodFalse(ctx)
+			return
 		}
+
+		// Handle chatmod false or not provided case (original format)
+		handleOriginalFormat(ctx)
 	})
 
 	endless.ListenAndServe(HOST+":"+PORT, handler)
 }
 
-func handleChatmodTrue(ctx *gin.Context) {
-	var search typings.Search
-	search.Query = ctx.Query("query")
-
-	if search.Query == "" {
-		ctx.JSON(400, gin.H{"error": "Query is required"})
-		return
-	}
-
-	// Get results
-	results, err := duckduckgo.Get_results(search)
-	if err != nil {
-		ctx.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Build the response in chatmod format
-	var prompt string
-	prompt += search.Query + ";"
-
-	for _, result := range results {
-		prompt += result.Title + ";" + result.Snippet + ";"
-	}
-
-	ctx.JSON(200, gin.H{"prompt": prompt})
-}
-
-func handleChatmodFalse(ctx *gin.Context) {
-	var search typings.Search
-
+func handleOriginalFormat(ctx *gin.Context) {
 	// Map request to Search struct
+	var search typings.Search
 	// Get query
 	search.Query = ctx.Query("query")
 	// Get region
@@ -98,4 +85,34 @@ func handleChatmodFalse(ctx *gin.Context) {
 	}
 	// Return results
 	ctx.JSON(200, results)
+}
+
+func handleChatmodTrue(ctx *gin.Context) {
+	// Get the query parameter
+	query := ctx.Query("query")
+
+	// Ensure query is set
+	if query == "" {
+		ctx.JSON(400, gin.H{"error": "Query is required"})
+		return
+	}
+
+	// Get results
+	results, err := duckduckgo.Get_results(typings.Search{Query: query})
+	if err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create a prompt string from the results
+	prompt := query + ";"
+	for _, result := range results {
+		prompt += result.Snippet + ";"
+	}
+
+	// Remove the trailing semicolon
+	prompt = prompt[:len(prompt)-1]
+
+	// Return the chatmod format response
+	ctx.JSON(200, gin.H{"prompt": prompt})
 }
