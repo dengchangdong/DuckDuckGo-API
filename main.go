def931a0e7cdb54f18177da352a8f400cf1bc136
Deng Changdong <dengchangdong@gmail.com>
2023-09-12 20:47:06 +0800

    Update main.go

--- main.go
+++ main.go
@@ -21,98 +21,65 @@ func main() {
 		c.String(200, "pong")
 	})
 	handler.POST("/search", func(ctx *gin.Context) {
-		// Check if chatmod parameter is set to true
-		chatmod := ctx.DefaultQuery("chatmod", "no")
-
-		if chatmod == "true" {
-			// Handle chatmod true case
-			handleChatmodTrue(ctx)
+		// Map request to Search struct
+		var search typings.Search
+		if err := ctx.ShouldBindJSON(&search); err != nil {
+			ctx.JSON(400, gin.H{"error": err.Error(), "details": "Could not bind JSON"})
 			return
 		}
-
-		// Handle chatmod false or not provided case (original format)
-		handleOriginalFormat(ctx)
+		// Ensure query is set
+		if search.Query == "" {
+			ctx.JSON(400, gin.H{"error": "Query is required"})
+			return
+		}
+		// Get results
+		results, err := duckduckgo.Get_results(search)
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		// Limit
+		if search.Limit > 0 && search.Limit < len(results) {
+			results = results[:search.Limit]
+		}
+		// Return results
+		ctx.JSON(200, results)
 	})
-
 	handler.GET("/search", func(ctx *gin.Context) {
-		// Check if chatmod parameter is set to true
-		chatmod := ctx.DefaultQuery("chatmod", "no")
-
-		if chatmod == "true" {
-			// Handle chatmod true case
-			handleChatmodTrue(ctx)
+		// Map request to Search struct
+		var search typings.Search
+		// Get query
+		search.Query = ctx.Query("query")
+		// Get region
+		search.Region = ctx.Query("region")
+		// Get time range
+		search.TimeRange = ctx.Query("time_range")
+		if search.Query == "" {
+			ctx.JSON(400, gin.H{"error": "Query is required"})
 			return
 		}
-
-		// Handle chatmod false or not provided case (original format)
-		handleOriginalFormat(ctx)
+		// Get limit and check if it's a number
+		limit := ctx.Query("limit")
+		if limit != "" {
+			if _, err := strconv.Atoi(limit); err != nil {
+				ctx.JSON(400, gin.H{"error": "Limit must be a number"})
+				return
+			}
+			search.Limit, _ = strconv.Atoi(limit)
+		}
+		// Get results
+		results, err := duckduckgo.Get_results(search)
+		if err != nil {
+			ctx.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		// Shorten results to limit if limit is set
+		if search.Limit > 0 && search.Limit < len(results) {
+			results = results[:search.Limit]
+		}
+		// Return results
+		ctx.JSON(200, results)
 	})
 
 	endless.ListenAndServe(HOST+":"+PORT, handler)
-}
-
-func handleOriginalFormat(ctx *gin.Context) {
-	// Map request to Search struct
-	var search typings.Search
-	// Get query
-	search.Query = ctx.Query("query")
-	// Get region
-	search.Region = ctx.Query("region")
-	// Get time range
-	search.TimeRange = ctx.Query("time_range")
-	if search.Query == "" {
-		ctx.JSON(400, gin.H{"error": "Query is required"})
-		return
-	}
-	// Get limit and check if it's a number
-	limit := ctx.Query("limit")
-	if limit != "" {
-		if _, err := strconv.Atoi(limit); err != nil {
-			ctx.JSON(400, gin.H{"error": "Limit must be a number"})
-			return
-		}
-		search.Limit, _ = strconv.Atoi(limit)
-	}
-	// Get results
-	results, err := duckduckgo.Get_results(search)
-	if err != nil {
-		ctx.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-	// Shorten results to limit if limit is set
-	if search.Limit > 0 && search.Limit < len(results) {
-		results = results[:search.Limit]
-	}
-	// Return results
-	ctx.JSON(200, results)
-}
-
-func handleChatmodTrue(ctx *gin.Context) {
-	// Get the query parameter
-	query := ctx.Query("query")
-
-	// Ensure query is set
-	if query == "" {
-		ctx.JSON(400, gin.H{"error": "Query is required"})
-		return
-	}
-
-	// Get results
-	results, err := duckduckgo.Get_results(typings.Search{Query: query})
-	if err != nil {
-		ctx.JSON(500, gin.H{"error": err.Error()})
-		return
-	}
-
-	// Create a prompt string from the results
-	prompt := query + ";"
-	for _, result := range results {
-		prompt += result.Snippet + ";"
-	}
-
-	// Remove the trailing semicolon
-	prompt = prompt[:len(prompt)-1]
-
-	// Return the chatmod format response
-	ctx.JSON(200, gin.H{"prompt": prompt})
 }
