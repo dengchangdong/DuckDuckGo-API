12cccc327730cea02567c5c5d677dd5ae05d1cb8
Deng Changdong <dengchangdong@gmail.com>
2023-09-14 21:50:46 +0800

    Update types.go

--- typings/types.go
+++ typings/types.go
@@ -5,7 +5,6 @@ type Search struct {
 	Region    string `json:"region"`
 	TimeRange string `json:"time_range"`
 	Limit     int    `json:"limit"`
-	Merge      bool   `json:"merge"`
 }
 
 type Result struct {
