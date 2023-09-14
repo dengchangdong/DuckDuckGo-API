abaa94af9d10331ac8d61fffd086580a5427cf4d
Deng Changdong <dengchangdong@gmail.com>
2023-09-14 21:55:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/acheong08/DuckDuckGo-API/duckduckgo"
-	"github.com/acheong08/DuckDuckGo-API/typings"
+	"github.com/dengchangdong/DuckDuckGo-API/duckduckgo"
+	"github.com/dengchangdong/DuckDuckGo-API/typings"
 	"github.com/acheong08/endless"
 	"github.com/gin-gonic/gin"
 )
