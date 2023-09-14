1a25bf6eb2f9e17fc7ae0a4a9e4c5965b6972690
Deng Changdong <dengchangdong@gmail.com>
2023-09-14 23:15:14 +0800

    Update main.go

--- main.go
+++ main.go
@@ -17,70 +17,47 @@ func main() {
 		PORT = "8080"
 	}
 	handler := gin.Default()
-
-	handler.GET("/search/ping", func(c *gin.Context) {
+	handler.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
-
 	handler.POST("/search", func(ctx *gin.Context) {
-
 		// Map request to Search struct
 		var search typings.Search
 		if err := ctx.ShouldBindJSON(&search); err != nil {
 			ctx.JSON(400, gin.H{"error": err.Error(), "details": "Could not bind JSON"})
 			return
 		}
-
 		// Ensure query is set
 		if search.Query == "" {
 			ctx.JSON(400, gin.H{"error": "Query is required"})
 			return
 		}
-
 		// Get results
 		results, err := duckduckgo.Get_results(search)
 		if err != nil {
 			ctx.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
-
 		// Limit
 		if search.Limit > 0 && search.Limit < len(results) {
 			results = results[:search.Limit]
 		}
-
 		// Return results
-		// default: ctx.JSON(200, results)
-		resultList := ctx.Query("list")
-		if resultList == "true" {
-			ctx.JSON(200, results)
-		} else {
-			var resultString string
-			for _, result := range results {
-				resultString += result.Snippet + "\n"
-			}
-			ctx.JSON(200, gin.H{"result": resultString})
-		}
+		ctx.JSON(200, results)
 	})
 	handler.GET("/search", func(ctx *gin.Context) {
-
 		// Map request to Search struct
 		var search typings.Search
-
 		// Get query
 		search.Query = ctx.Query("query")
-
 		// Get region
 		search.Region = ctx.Query("region")
-
 		// Get time range
 		search.TimeRange = ctx.Query("time_range")
-
 		if search.Query == "" {
 			ctx.JSON(400, gin.H{"error": "Query is required"})
 			return
 		}
-
 		// Get limit and check if it's a number
 		limit := ctx.Query("limit")
 		if limit != "" {
@@ -90,31 +67,18 @@ func main() {
 			}
 			search.Limit, _ = strconv.Atoi(limit)
 		}
-
 		// Get results
 		results, err := duckduckgo.Get_results(search)
 		if err != nil {
 			ctx.JSON(500, gin.H{"error": err.Error()})
 			return
 		}
-
 		// Shorten results to limit if limit is set
 		if search.Limit > 0 && search.Limit < len(results) {
 			results = results[:search.Limit]
 		}
-
 		// Return results
-		// default: ctx.JSON(200, results)
-		resultList := ctx.Query("list")
-		if resultList == "true" {
-			ctx.JSON(200, results)
-		} else {
-			var resultString string
-			for _, result := range results {
-				resultString += result.Snippet + "\n"
-			}
-			ctx.JSON(200, gin.H{"result": resultString})
-		}
+		ctx.JSON(200, results)
 	})
 
 	endless.ListenAndServe(HOST+":"+PORT, handler)
